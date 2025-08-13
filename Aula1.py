46a47e6867c09ea075ccc49912d106c7b45a7c11
4lkm1m <arthur.braga.a@loyola.g12.br>
2025-08-13 14:52:36 -0300

    Create Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,7 @@
+num1=int(input ("digite a primeira nota: "))
+num2=int(input ("digite a segunda nota: "))
+num3=int(input ("digite a terceira nota: "))
+num4=int(input ("digite a quarta nota: "))
+resultado = 0
+resultado = (num1 + num2 + num3 + num4)/4
+print(resultado)
